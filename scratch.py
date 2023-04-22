45122a97a40f1ae0ffe42c4d4c9a26bc4da684d8
Christina Zhou <christina.zhou.96@gmail.com>
2023-04-22 17:48:55 -0400

    second attempt at graph code

--- scratch.py
+++ scratch.py
@@ -16,6 +16,16 @@ decision_tree_accuracies = []
 decision_stump_accuracies = []
 dt3_accuracies = []
 
+classifiers = {
+    'Decision Tree': tree.DecisionTreeClassifier(),
+    'Decision Stump': tree.DecisionTreeClassifier(max_depth=1),
+    '3-level Decision Tree': tree.DecisionTreeClassifier(max_depth=3)
+}
+
+# Initialize arrays to store accuracy and learning curve data
+accuracies = {name: [] for name in classifiers.keys()}
+learning_curve_data = {name: [] for name in classifiers.keys()}
+
 # Perform 100 trials
 for trial in range(100):
     # shuffle the data
@@ -31,7 +41,7 @@ for trial in range(100):
     current_dt3_accuracies = []
 
     # 10-fold cross-validation
-    fold_size = n //10
+    fold_size = n // 10
     for i in range(10):
         # Test
         X_test = X[i * fold_size:(i + 1) * fold_size]
@@ -63,10 +73,41 @@ for trial in range(100):
         current_decision_stump_accuracies.append(accuracy_score(y_test, y_pred_decision_stump))
         current_dt3_accuracies.append(accuracy_score(y_test, y_pred_dt3))
 
+        # Calculate learning curve data for each classifier and different training set sizes
+        for train_size_percentage in range(10, 101, 10):
+            train_size = int(train_size_percentage * fold_size / 100)
+            X_train_sub = X_train[:train_size, :]
+            y_train_sub = y_train[:train_size, :]
+
+            for classifier_name, classifier in classifiers.items():
+                clf = classifier.fit(X_train_sub, y_train_sub)
+                y_pred = clf.predict(X_test)
+                accuracy = accuracy_score(y_test, y_pred)
+
+                if trial == 0 and i == 0:
+                    learning_curve_data[classifier_name].append([accuracy])
+                else:
+                    learning_curve_data[classifier_name][train_size_percentage // 10 - 1].append(accuracy)
+
     # Store the mean accuracy for each classifier in the current trial
     decision_tree_accuracies.append(np.mean(current_decision_tree_accuracies))
     decision_stump_accuracies.append(np.mean(current_decision_stump_accuracies))
     dt3_accuracies.append(np.mean(current_dt3_accuracies))
+
+accuracies = {'Decision Tree':decision_tree_accuracies,
+              'Decision Stump':decision_stump_accuracies,
+              '3-Level Decision Tree':dt3_accuracies}
+
+# Calculate mean and standard deviation for accuracy and learning curve data
+stats = {name: (np.mean(acc), np.std(acc)) for name, acc in accuracies.items()}
+for name in learning_curve_data.keys():
+    learning_curve_data[name] = np.array([np.mean(accuracies), np.std(accuracies)] for accuracies in learning_curve_data[name])
+
+plt.figure()
+for name, data in learning_curve_data.items():
+    plt.errorbar(range(10, 101, 10), data[:, 0], yerr=data[:, 1], label=name)
+
+plt.show()
 
 # Calculate mean and standard deviation for each classifier
 mean_decision_tree_accuracy = np.mean(decision_tree_accuracies)
--- scratch3.py
+++ scratch3.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 
-
 filename = 'data/SPECTF.dat'
 data = np.loadtxt(filename, delimiter=',')
 X = data[:, 1:]
@@ -16,26 +15,23 @@ classifiers = {
     '3-level Decision Tree': tree.DecisionTreeClassifier(max_depth=3)
 }
 
-# Initialize arrays to store accuracy and learning curve data
 accuracies = {name: [] for name in classifiers.keys()}
 learning_curve_data = {name: [] for name in classifiers.keys()}
 
-# Perform 100 trials
 for trial in range(100):
     idx = np.arange(n)
     np.random.shuffle(idx)
     X = X[idx]
     y = y[idx]
 
-    # 10-fold cross-validation
     fold_size = n // 10
     for i in range(10):
-        X_test = X[i * fold_size:(i + 1) * fold_size, :]
-        y_test = y[i * fold_size:(i + 1) * fold_size, :]
-        X_train = np.concatenate((X[:i * fold_size, :], X[(i + 1) * fold_size:, :]), axis=0)
-        y_train = np.concatenate((y[:i * fold_size, :], y[(i + 1) * fold_size:, :]), axis=0)
+        X_test = X[i * fold_size:(i + 1) * fold_size]
+        y_test = y[i * fold_size:(i + 1) * fold_size]
 
-        # Calculate accuracy and learning curve data for each classifier
+        X_train = np.concatenate((X[:i * fold_size], X[(i + 1) * fold_size:]), axis=0)
+        y_train = np.concatenate((y[:i * fold_size], y[(i + 1) * fold_size:]), axis=0)
+
         for name, clf in classifiers.items():
             clf = clf.fit(X_train, y_train)
             y_pred = clf.predict(X_test)
@@ -43,7 +39,6 @@ for trial in range(100):
 
             accuracies[name].append(accuracy)
 
-            # Calculate learning curve data for different training set sizes
             for train_size_percentage in range(10, 101, 10):
                 train_size = int(train_size_percentage * fold_size / 100)
                 X_train_sub = X_train[:train_size, :]
@@ -58,24 +53,19 @@ for trial in range(100):
                 else:
                     learning_curve_data[name][train_size_percentage // 10 - 1].append(accuracy)
 
-# Calculate mean and standard deviation for accuracy and learning curve data
-stats = {name: (np.mean(acc), np.std(acc)) for name, acc in accuracies.items()}
 for name in learning_curve_data.keys():
     learning_curve_data[name] = np.array([np.mean(accuracies), np.std(accuracies)] for accuracies in learning_curve_data[name])
 
-def plot_learning_curve(learning_curve_data):
-    plt.figure()
-    for name, data in learning_curve_data.items():
-        plt.errorbar(range(10, 101, 10), data[:, 0], yerr=data[:, 1], label=name)
+plt.figure()
+for name, data in learning_curve_data.items():
+    plt.errorbar(range(10, 101, 10), data[:, 0], yerr=data[:, 1], label=name)
 
-    plt.xlabel('Training Data Percentage')
-    plt.ylabel('Test Accuracy')
-    plt.title('Learning Curve')
-    plt.legend()
-    plt.grid()
-    plt.show()
+plt.xlabel('Training Data Percentage')
+plt.ylabel('Test Accuracy')
+plt.title('Learning Curve')
+plt.legend()
+plt.grid()
+plt.show()
 
-plot_learning_curve(learning_curve_data)
-
-
-
+stats = {name: (np.mean(acc), np.std(acc)) for name, acc in accuracies.items()}
+print(stats)
